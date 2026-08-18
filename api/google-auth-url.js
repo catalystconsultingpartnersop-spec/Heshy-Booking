883d5d6551fac986207d30487f72224ac61cf738
catalystconsultingpartnersop-spec <catalystconsultingpartners.op@gmail.com>
2026-08-17 21:00:45 -0400

    Add files via upload

--- api/google-auth-url.js
+++ api/google-auth-url.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
   }
   const clientId = process.env.GOOGLE_CLIENT_ID;
   const redirectUri = process.env.GOOGLE_REDIRECT_URI;
-  const scope = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/drive.readonly';
+  const scope = 'https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/meetings.space.created';
   const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${encodeURIComponent(clientId)}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&access_type=offline&prompt=consent&scope=${encodeURIComponent(scope)}&state=${encodeURIComponent(token)}`;
   res.writeHead(302, { Location: url });
   res.end();
